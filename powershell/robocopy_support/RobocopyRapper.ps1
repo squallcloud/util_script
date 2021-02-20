8a4d24195e599a785daa93fa1f1a95dae6f19865
squallcloud <squallcloudzidane@gmail.com>
2021-02-20 20:55:32 +0900

    feat: robocopyのラッパーを作成

---
+++ powershell/robocopy_support/RobocopyRapper.ps1
@@ -0,0 +1,21 @@
+﻿# robocopyをラップしたクラス
+class RobocopyRapper {
+    # コマンド名
+    [String] $m_commandName = "robocopy"
+
+    # 実行した際のプロセス
+    [System.Diagnostics.Process] $m_process = $null
+
+    RobocopyRapper() {
+    }
+
+    # 実行
+    [int] Exec($in_src, $in_dst) {
+        $proc = Start-Process -FilePath $this.m_commandName -ArgumentList "`"${in_src}`" `"${in_dst}`"" -NoNewWindow -Wait
+        return $proc.ExitCode
+    }
+    [int] ExecMir($in_src, $in_dst) {
+        $proc = Start-Process -FilePath $this.m_commandName -ArgumentList "`"${in_src}`" `"${in_dst}`" /MIR" -NoNewWindow -Wait
+        return $proc.ExitCode
+    }
+}
