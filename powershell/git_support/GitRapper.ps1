fa2b5a88e3db1b6b4643602e67f787b66946b4fe
squallcloud <squallcloudzidane@gmail.com>
2021-02-21 22:54:46 +0900

    Gitのpowershellラッパー作成 (#2)
    * gitのラッパーを作成
    
    * コミット時のコメント設定処理を修正
    
    * テスト用ファイル追加

---
+++ powershell/git_support/GitRapper.ps1
@@ -0,0 +1,22 @@
+﻿# gitをラップしたクラス
+class GitRapper {
+    # コマンド名
+    [String] $m_commandName = "git"
+
+    GitRapper() {
+    }
+
+    # 実行
+    [int] ExecAdd($in_path) {
+        $proc = Start-Process -FilePath $this.m_commandName -ArgumentList "add `"${in_path}`"" -NoNewWindow -Wait
+        return $proc.ExitCode
+    }
+    [int] ExecCommit($in_comments) {
+        $comment = ""
+        foreach ($c in $in_comments) {
+            $comment = "${comment} -m `"${c}`""
+        }
+        $proc = Start-Process -FilePath $this.m_commandName -ArgumentList "commit ${comment}" -NoNewWindow -Wait
+        return $proc.ExitCode
+    }
+}
---
+++ powershell/git_support/sample.ps1
@@ -0,0 +1,5 @@
+﻿. .\GitRapper.ps1
+
+$mod = New-Object GitRapper
+$mod.ExecAdd(".\test.txt")
+$mod.ExecCommit(@("コメントテスト1", "コメントテスト2"))
