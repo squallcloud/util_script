0c69eda1e9f8f67b61bfc65c293154f1911f4937
squallcloud <squallcloudzidane@gmail.com>
2021-02-21 22:27:01 +0900

    gitのラッパーを作成

---
+++ powershell/git_support/GitRapper.ps1
@@ -0,0 +1,18 @@
+﻿# gitをラップしたクラス
+class GitRapper {
+    # コマンド名
+    [String] $m_commandName = "git"
+
+    GitRapper() {
+    }
+
+    # 実行
+    [int] ExecAdd($in_path) {
+        $proc = Start-Process -FilePath $this.m_commandName -ArgumentList "add `"${in_path}`"" -NoNewWindow -Wait
+        return $proc.ExitCode
+    }
+    [int] ExecCommit($in_path, $in_comment) {
+        $proc = Start-Process -FilePath $this.m_commandName -ArgumentList "commit `"${in_path}`" -m `"${in_comment}`"" -NoNewWindow -Wait
+        return $proc.ExitCode
+    }
+}
